09db8d8e3f2094bd1f1b63718938878e0a30ef8f
Justin Dennison <justin1dennison@gmail.com>
2020-08-17 05:40:23 -0400

    cleanup

--- examples/sample.js
+++ examples/sample.js
@@ -1,8 +1,14 @@
 (async function main() {
+   const jsonify = (obj) => JSON.stringify(obj, null, 2)
+   const select = (...keys) => obj => keys.reduce((acc, k) => {
+       acc[k] = obj[k]
+       return acc
+   }, {})
    const path = require('path')
    const {  Grib } = require('../dist/lib')
    const filepath = path.resolve(__dirname, '../data/sample.grib2')
    const ds = await Grib.fromFile(filepath)
-   console.log({ ds })
-   console.log(ds.messages[0])
+   console.log(ds.messages.map(select('identification', 'indicator')).map(jsonify).join())
 })().catch(console.error)
+
+
